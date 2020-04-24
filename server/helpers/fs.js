d5770d3bf720a2e9dc9a005d0c1ae0893dd1d707
zhd4nov <prctmlm@gmail.com>
2020-04-24 14:11:03 +0300

    feture(server) implement basic features

--- server/helpers/fs.js
+++ server/helpers/fs.js
@@ -23,7 +23,7 @@ export function readFile(filename) {
           return;
         }
       }
-    )
+    );
   });
 }
 
--- server/index.js
+++ server/index.js
@@ -22,6 +22,20 @@ app.get('/users', async (req, res) => {
   res.json(users);
 });
 
+const server = app.listen(port, () => {
+  console.log(`Server up and running on port ${port}`)
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('user connected', socket.id);
+
+  socket.on('disconnect', (socket) => {
+    console.log('user disconnected', socket.id);
+  })
+})
+
 async function getUsersData() {
   const data = await readFile('users.json');
   return data;

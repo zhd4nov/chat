41ba027c1def462fdf8dd8ea122cad50faf86f26
zhd4nov <prctmlm@gmail.com>
2020-05-02 20:22:50 +0300

    chore(api) mark line to refactor

--- app/components/User/userRouter.js
+++ app/components/User/userRouter.js
@@ -3,6 +3,6 @@ const userController = require('./userController');
 
 // ORDER IS IMPORTANT (or pain)
 userRouter.use('/invite/:chatId', userController.welcomeFriend);
-userRouter.use('/', userController.getUsers);
+userRouter.use('/', userController.getUsers); // TODO: /all, app should work fine
 
 module.exports = userRouter;

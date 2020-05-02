8717b115dce68e47210f0cf34d98df67484ba8f7
zhd4nov <prctmlm@gmail.com>
2020-05-02 20:24:23 +0300

    chore(app state) add action handler for users

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,36 @@
 import { handleActions } from 'redux-actions';
 import { combineReducers } from 'redux';
 
-const defaultState = { AllIds: {}, ByIds: [] };
+import * as actions from '../actions';
+
+const defaultState = { ByIds: {}, AllIds: [] };
 const defaultStateForCurrent = { id: '' };
 const defaultUIState = {};
 
 
-const chats = handleActions({}, defaultState);
-const users = handleActions({}, defaultState);
-const messages = handleActions({}, defaultState);
+const chats = handleActions(
+  {},
+  defaultState,
+);
+
+const users = handleActions(
+  {
+    [actions.updateUserList]: (state, { payload: { users } }) => { // users is [objects]
+      // Create array with all user ids
+      const AllIds = users.map((user) => user.id);
+      // Create object with users where key is user id and value is user object
+      const ByIds = users.reduce((acc, user) => ({ ...acc, [user.id]: user }), {});
+
+      return { ByIds, AllIds };
+    }
+  },
+  defaultState,
+);
+
+const messages = handleActions(
+  {},
+  defaultState,
+);
 
 const currentUser = handleActions({}, defaultStateForCurrent);
 const currentChat = handleActions({}, defaultStateForCurrent);
